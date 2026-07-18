ef8a74aae295d0ab7b2d1d8294da9253f62a396b
Ravi Gupta <ravikgupta32@gmail.com>
2026-07-18 18:37:06 +0530

    If else and String basics

---
+++ Python/Fundamentals/Conditionals/ifelse.py
@@ -0,0 +1,27 @@
+#If-else control statement
+x= 13
+if(x==10):
+    print ('x is',x)
+else:
+    print('x is',x)
+
+#If else and elif
+x=110;
+if (x==10):
+    print('X is 10')
+elif(x>100):
+    print('X is greater than 100')
+else:
+    print('X is not 10')
+
+#Nested if else
+x=210;
+if (x==10):
+    print('X is 10')
+elif(x>100):
+    if(x>100 and x<200):
+        print('Between 100 & 200')
+    else:
+        print('Greater than 200')
+else:
+    print('X is not 10')
--- Python/Fundamentals/string.py
+++ Python/Fundamentals/string.py
@@ -5,3 +5,33 @@ print(x[0])
 print(x[0:4])
 #Last 4 character 
 print(x[4:])
+#No of elements in a string array
+print(len(x))
+print(x[4:len(x)])
+
+#String methods
+x='Hello World'
+print(x.upper())
+print(x.lower())
+print(x.capitalize())
+print(x.replace('H','z').capitalize())
+
+letter='A new random keyword'
+my_list = letter.split(" ")
+print(my_list)
+file_name = 'raw_data.csv'
+if (file_name.endswith('.csv')):
+    print('CSV File')
+
+if(file_name.startswith('raw')):
+    print('Raw File')
+statement ="Hello User. What are you doing. Hey User I am talking to you."
+
+print(statement.count('User'))
+
+#is functions()
+
+demo_str='Hello'
+demo_var ="10abc"
+print(demo_str.isnumeric())
+print(demo_var.isalnum())
