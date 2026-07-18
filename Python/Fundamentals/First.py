0bb7c2f29fdb233273a88d8459dc9d367bb35718
Ravi Gupta <ravikgupta32@gmail.com>
2026-07-18 14:34:30 +0530

    Python Learning first file

---
+++ Python/Fundamentals/First.py
@@ -0,0 +1,14 @@
+print("Hello Ravi")
+# Step1: When we run a .py., python compiles the entire script into an intermediate form called bytecode(a. pyc file)
+# Step 2: PVM(Python virtual machine) takes over and the bytecode is executed line by line but this part happens during runtime.
+#Python is considered an interpreted language
+#Multiple types
+print("Hello Ravi",1,"Test")
+# with seperator
+print("Hello Ravi",100,"Is the number",sep='-') 
+#for multiple line print
+print('''Hello ravi
+how is your day''')
+#Using Quotes
+print("Hello 'Ravi'. how are you")
+print('Hello \'Ravi\'. how are you')
